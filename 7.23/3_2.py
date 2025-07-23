9a9bb22ff4084358b0d4442d15b11fefa49b6fa4
Seungjae Bang <bangsky999@naver.com>
2025-07-23 15:50:42 +0900

    실습미완성

---
+++ 7.23/3_2.py
@@ -0,0 +1,24 @@
+number_of_people = 0
+
+def increase_user():
+    global number_of_people
+    number_of_people += 1
+
+def create_user():
+    
+    increase_user()
+    user_info = {}
+    user_info['name'] = '홍길동'
+    user_info['age'] = 30
+    user_info['adress'] = '서울'
+    print(f"{user_info['name']}님 환영합니다!")
+    return user_info
+
+    
+    
+
+print(number_of_people)
+# create_user()
+result = create_user()
+print(result)
+print(number_of_people)
