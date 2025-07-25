3e69c2033a2512bb9e00adf55d06bb4a451b0865
Seungjae Bang <bangsky999@naver.com>
2025-07-25 13:33:55 +0900

    0724 reupload

--- 7.25/requests_test.py
+++ 7.25/requests_test.py
@@ -1,6 +1,6 @@
 import requests
 import pprint
 url = "https://fakestoreapi.com/carts"
-data = requests.get(url).json() # 조회 요청
+data = requests.get(url).json() # 조회 요청 # json()은 내부 데이터를 딕셔너리 형태로 ㅂ ㅕㄴ환
 pprint.pprint(data) # 200: 정상 , 404: 그런 데이터는 우리 서버에 없다
 
