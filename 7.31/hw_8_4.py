db33c6b85c7a938e18685657e235ebc1956eed61
unknown <bangsky999>
2025-08-03 22:37:26 +0900

    hw_8_4 수정

--- 7.31/hw_8_4.py
+++ 7.31/hw_8_4.py
@@ -1,16 +1,15 @@
 # 아래 클래스를 수정하시오.
 class UserInfo:
     user_dict = {}
-    def __init__(self): # 인스턴스 변수가 없는데 생성자에 변수 설정 how????????????
-        # self.name = name
-        # self.age = age
+    def __init__(self): 
         self.value = False
+    # "self.변수 = 변수"가 필수적이다 => 생성자 메서드에서 생성
 
-    def get_user_info(self): # 여기는 생성자 변수 입력안해도되나??????????
-        
+    # "self.변수 = 변수"가 추가적으로 필요하더라 => 메서드에서 사용할 수도 있음
+    def get_user_info(self):
         try: # 에러가 나는 코드를 발생시킨다.
-            self.name = input('이름을 입력하세요 : ') # name, age 변수를 클래스 내에서 공유하려면 self.형식?????
-            self.age = int(input('나이을 입력하세요 : ')) # self의 역할이 뭐지 그럼??????
+            self.name = input('이름을 입력하세요 : ')
+            self.age = int(input('나이을 입력하세요 : '))
         
         except ValueError: # 입력값 에러 발생시
             print('나이는 숫자로 입력해야 합니다.')
@@ -31,7 +30,7 @@ class UserInfo:
             print('사용자 정보가 입력되지 않았습니다.')
 
 user = UserInfo()
-# print(user.name)
+
 user.get_user_info()
 user.display_user_info()
 print(user.name)
